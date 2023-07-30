c33eb96512cf70c684b0707b288856776814e7d1
Solaraeth <bpwandera@gmail.com>
2023-07-31 00:11:41 +0100

    Edit more commands, comments, readability
    - `stats` and `commands` inputs now have effects
    - Code now contains comments
    - Various formatting changes to imporve readability
    - Still need to add conditionals to chimchar-user intractions.
    - This includes
        - Max Values
        - Relationships between stats

--- chimchar.cpp
+++ chimchar.cpp
@@ -37,11 +37,15 @@
 
 int main() {
     
+    // Variables
     std::string nickname;
+    std::string command;
+    
+    // Vectors
     std::vector<std::string> stats = {"Level", "Hunger", "Happiness"};
     std::vector<int> statValue = {1, 1, 1};
-    std::string command;
-
+    
+    // Introduction Message + Chimchar nickname input
     std::cout << "Welcome to Chimchar Care.\n\n"
 
                 "You've been entrusted with raising a Chimchar, a fire-type chimp Pokemon. Your goal is to keep Chimchar happy and healthy\n"
@@ -54,6 +58,7 @@ int main() {
                 "First give your Chimchar a nickname: ";
     std::cin >> nickname;
 
+    // Explanation to User of game mechanics + commands
     std::cout << "\n" << nickname << "loves his new name! I think you'll get along great!\n\n"
 
                 "Now it's time to learn how to take care of " << nickname << ".\n\n"
@@ -66,42 +71,71 @@ int main() {
 
                 "Type 'stats' at any time to review " << nickname << "'s hunger, happiness and level.\n"
                 "These are their current Values:\n"
-                << stats[0] << ": " << statValue[0] << " " << stats[1] << ": " << statValue[1] << " " << stats [2] << ": "<< statValue[0] << "\n\n"
+                << stats[0] << "-" << statValue[0] << " " << stats[1] << "-" << statValue[1] << " " << stats [2] << "-"<< statValue[0] << "\n\n"
                 
                 "Type 'commands' at any time to review commands and their effects.\n\n"
                 
                 "If you balance " << nickname << "'s needs right, " << nickname << " will evolve in no time.\n\n"
                 
-                "Now let's get started with caring for your new friend!\n";
+                "Now let's get started with caring for your new friend!\n\n";
+
+    //Main game while loop
     while(statValue[0] < 5) {
 
+        //Asks user to input chimchar interaction command
         std::cout << "What do you want to do with " << nickname << " : ";
         std::cin >> command;
 
+        // If user types "feed" incresases hunger by 1
         if (command == "feed") {
 
             statValue[1] = statValue[0] + 1;
 
         }
 
+        // If user types "play" increases happiness by 1
         else if (command == "play") {
 
             statValue[2] = statValue[2] + 1;
         }
 
+        // If user types "train" increases level by 1
         else if (command == "train") {
 
             statValue[0] = statValue[0] + 1;
             std::cout << "Nice! " << nickname << " is now Lvl " << statValue[0] << "!\n";
         }
 
+        // If user types "stats" reveals chimchar's current stat values
+        else if (command == "stats") {
+
+            std::cout << "\nThese are " << nickname << "'s current stats:\n" 
+            << stats[0] << " - " << statValue[0] << "\n" << stats[1] << " - " << statValue[1] << "\n" << stats [2] << " - "<< statValue[0] << "\n\n";
+        }
+
+        // If user types "commands" prints list of valid inputs and their effects
+        else if (command == "commands") {
+
+            std::cout << "\nCommands:\n"
+                        "feed: Increases " << nickname << "'s hunger stat by 1\n"
+                        "play: Increases " << nickname << "'s happiness stat by 1\n"
+                        "train: Increases " << nickname << "'s level  by 1\n"
+                        "stats: Reveals " << nickname << "'s current stats\n"
+                        "commands: Shows the list of commands and their effects\n";
+
+        }
+
+        // Error message for invalid command inputs from user
         else {
 
             std::cout << "Invalid input\n";
         }
-
+        
+        // Improves readability
+        std::cout << "\n";
     }
     
+    //Congratulations message to user for completing Game
     std::cout << "Woah! " << nickname << " is evolving\n\n"
 
                 "Congratulations, you've done it! Your excellent care and dedication has helped " << nickname << " evolve into Infernape\n\n"
